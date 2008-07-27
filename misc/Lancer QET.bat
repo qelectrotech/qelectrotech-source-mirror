0272d5ed6638df192006a8abaa14c52732c8c6db
xavierqet <xavierqet@r9  xavierqet  2006-10-30 20:28:16 +0100 (lun. 30 oct. 2006)>
2008-07-27 15:48:06 +0000

    Mise a jour des associations de fichiers sous Windows
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@358 bfdf4180-ca20-0410-9c96-a3a8aa849046

---
+++ misc/Lancer QET.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem Se rend dans le dossier qui convient
+set current_dir=%~dp0
+cd /d %current_dir%
+
+rem Met la collection QET en lecture seule
+attrib +r elements/* /S /D
+
+rem lance QElectroTech
+set command=bin\qelectrotech.exe --common-elements-dir=elements/ --config-dir=conf/ -style plastique %*
+@start %command%
