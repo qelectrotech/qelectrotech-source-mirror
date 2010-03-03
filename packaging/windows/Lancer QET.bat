9fef71c62646a3d2c3fe2959705e26799b8da9c7
xavier <xavier@r991  xavier  2010-05-09 20:40:08 +0200 (dim. 09 mai 2010)>
2010-03-03 15:23:26 +0000

    Ameliorations de l'installeur Windows :   * les langages proposes par l'installeur sont desormais les memes que QElectroTech, pour la coherence   * la configuration est placee dans le home de l'utilisateur (%APPDATA%\qet\), et non plus dans le dossier d'installation   * les associations de fichiers sont mises en place par l'installeur
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@875 bfdf4180-ca20-0410-9c96-a3a8aa849046

---
+++ packaging/windows/Lancer QET.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem Se rend dans le dossier qui convient
+set current_dir=%~dp0
+cd /d %current_dir%
+
+rem Met la collection QET en lecture seule
+attrib +r elements/* /S /D
+
+rem lance QElectroTech
+rem Sans option --config-dir, la configuration de QElectroTech ainsi que la
+rem collection d'elements perso seront dans "%APPDATA%\qet"
+set command=bin\qelectrotech.exe --common-elements-dir=elements/ --lang-dir=lang/ -style plastique %*
+@start %command%
