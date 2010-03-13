c216ff786e4b2f1e786a889dc0cdb82c4a9ae8b4
xavier <xavier@r991  xavier  2010-05-09 20:40:08 +0200 (dim. 09 mai 2010)>
2010-03-13 17:32:41 +0000

    Modification de la version et du splash screen pour le passage en 0.21.
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@893 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/qet.h
+++ sources/qet.h
@@ -25,9 +25,9 @@
 */
 namespace QET {
 	/// version de QElectroTech (utilisee pour estampiller les projets et elements)
-	const QString version = "0.21";
+	const QString version = "0.22";
 	/// version affichee de QElectroTech
-	const QString displayedVersion = "0.21";
+	const QString displayedVersion = "0.22";
 	QString license();
 	/// Orientation (utilise pour les bornes mais aussi pour les elements)
 	enum Orientation {North, East, South, West};
