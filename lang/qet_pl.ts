c203371ffd69ac171a8a7119391580385c70a3eb
pawel32640 <pawel32640@r999  pawel32640  2010-05-24 11:06:45 +0200 (lun. 24 mai 2010)>
2010-08-01 18:26:14 +0000

    minor amendment of the polish translation
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/branches/0.3@1096 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- lang/qet_pl.ts
+++ lang/qet_pl.ts
@@ -2378,7 +2378,7 @@ Dostępne opcje:
     <message>
         <location filename="../sources/qetdiagrameditor.cpp" line="430"/>
         <source>Afficha&amp;ge</source>
-        <translation>&amp;Wyświetl</translation>
+        <translation>&amp;Widok</translation>
     </message>
     <message>
         <location filename="../sources/qetdiagrameditor.cpp" line="502"/>
