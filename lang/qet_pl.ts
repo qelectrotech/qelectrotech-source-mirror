7d1355d0ddfa824a5142c33e72343e5cab255285
pawel32640 <pawel32640@r999  pawel32640  2010-05-24 11:06:45 +0200 (lun. 24 mai 2010)>
2010-09-19 14:23:06 +0000

    minor corrections in the polish translation files
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@1109 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- lang/qet_pl.ts
+++ lang/qet_pl.ts
@@ -2499,7 +2499,7 @@ Dostępne opcje:
     <message>
         <location filename="../sources/qetdiagrameditor.cpp" line="428"/>
         <source>&amp;Édition</source>
-        <translation>&amp;Edytuj</translation>
+        <translation>&amp;Edycja</translation>
     </message>
     <message>
         <location filename="../sources/qetdiagrameditor.cpp" line="207"/>
