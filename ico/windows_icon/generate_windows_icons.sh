3fc74b8723eadb5f4ef9d4b3cbae38c95de2cbe3
xavier <xavier@r991  xavier  2010-05-09 20:40:08 +0200 (dim. 09 mai 2010)>
2012-05-08 20:33:07 +0000

    Added bitmap icons generated using first drafts from Nuno Pinheiro.
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@1767 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- ico/windows_icon/generate_windows_icons.sh
+++ ico/windows_icon/generate_windows_icons.sh
@@ -2,17 +2,18 @@
 # Dependance : png2ico
 PNG_DIR="../oxygen-icons"
 
-if [ -z "$(which png2ico 2> /dev/null)" ]; then
-	echo "Error: png2ico not found."
+if [ -z "$(which icotool 2> /dev/null)" ]; then
+	echo "Error: icotool not found."
 	exit 1
 fi
 
 function generateIcon {
 	echo "Generating icon from files ${1}.png in ${PNG_DIR}."
-	png2ico "${1}.ico" $(find "${PNG_DIR}" -name "${1}.png" | grep -v 2[25])
+	icotool -c -o "${1}.ico" $(find "${PNG_DIR}" -name "${1}.png" | grep -v 2[25])
 }
 
 generateIcon qelectrotech
 generateIcon application-x-qet-element
 generateIcon application-x-qet-project
+generateIcon application-x-qet-titleblock
 
