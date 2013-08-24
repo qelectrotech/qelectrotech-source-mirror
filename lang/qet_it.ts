6e47837a72807b31b63476e91add13ba484a56f9
silvio <silvio@r5890  silvio  2019-05-23 09:57:52 +0200 (jeu. 23 mai 2019)>
2013-08-24 18:08:27 +0000

    fixed Italian translation of new .ts file
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@2455 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- lang/qet_it.ts
+++ lang/qet_it.ts
@@ -2453,8 +2453,7 @@ I cambiamenti saranno irreversibili.</translation>
     <message>
         <location filename="../sources/ui/numparteditorw.ui" line="29"/>
         <source>Chiffre 1</source>
-        <translation>1 Cifra (1)
-</translation>
+        <translation type="unfinished">1 Cifra (1)</translation>
     </message>
     <message>
         <location filename="../sources/ui/numparteditorw.ui" line="34"/>
