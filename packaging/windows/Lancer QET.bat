caf9c3c096884e9f6dacea3bf8991b8632c50de3
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2014-01-19 15:35:41 +0000

    Bug fix: refresh Lancer QET.bat Add --common-tbt-dir=titleblocks/ to installer and change style
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@2755 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- packaging/windows/Lancer QET.bat
+++ packaging/windows/Lancer QET.bat
@@ -7,5 +7,5 @@ cd /d %current_dir%
 rem lance QElectroTech
 rem Sans option --config-dir, la configuration de QElectroTech ainsi que la
 rem collection d'elements perso seront dans "%APPDATA%\qet"
-set command=bin\qelectrotech.exe --common-elements-dir=elements/ --lang-dir=lang/ -style plastique %*
+set command=bin\qelectrotech.exe --common-elements-dir=elements/ --common-tbt-dir=titleblocks/ --lang-dir=lang/ -style windowsvista %*
 @start %command%
