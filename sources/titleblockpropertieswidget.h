6d1d4e3bb80370d630acc12555c8f49d551a4f9e
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2014-01-27 03:00:37 +0000

    Update Copyright
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@2780 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/titleblockpropertieswidget.h
+++ sources/titleblockpropertieswidget.h
@@ -1,5 +1,5 @@
 /*
-	Copyright 2006-2012 Xavier Guerrin
+	Copyright 2006-2013 The QElectroTech Team
 	This file is part of QElectroTech.
 
 	QElectroTech is free software: you can redistribute it and/or modify
