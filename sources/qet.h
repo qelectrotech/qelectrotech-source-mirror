48d6989ccee74878d5f204fba1120bc063bb92de
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2014-02-08 07:29:10 +0000

    Try Refresh svn keyword
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@2828 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/qet.h
+++ sources/qet.h
@@ -26,7 +26,7 @@ namespace QET {
 	/// QElectroTech version (as string, used to mark projects and elements XML documents)
 	const QString version = "0.4";
 	/// QElectroTech displayed version
-	const QString displayedVersion = "0.4-dev ($Rev::  $) ";
+	const QString displayedVersion = "0.4-dev-$Revision$";
 	QString license();
 	/// Orientation (used for electrical elements and their terminals)
 	enum Orientation {North, East, South, West};
