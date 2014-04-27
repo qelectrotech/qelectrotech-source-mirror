a8e7892546df2d5ae2a2f1b193e4cab933e68522
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2014-04-27 16:12:39 +0000

    Update genericpanel
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@3027 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/genericpanel.cpp
+++ sources/genericpanel.cpp
@@ -1226,7 +1226,8 @@ template<typename T>
 T GenericPanel::valueForItem(QTreeWidgetItem *item) const {
 	return(qVariantValue<T>(item -> data(0, GenericPanel::Item)));
 }
-
+template ElementsLocation GenericPanel::valueForItem(QTreeWidgetItem*)
+const;
 /**
 	
 */
