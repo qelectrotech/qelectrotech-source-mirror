c68e7ebbecf336faefc5d9596a3ff57abad9f7fe
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2014-06-21 09:51:37 +0000

    Restore it_prev_
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@3159 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/elementspanel.cpp
+++ sources/elementspanel.cpp
@@ -132,6 +132,11 @@ bool ElementsPanel::selectedItemIsWritable() const {
 	@return la collection, la categorie ou l'element selectionne(e)
 */
 ElementsCollectionItem *ElementsPanel::selectedItem() const {
+	// change color of drag element
+	if(it_prev_) {
+	it_prev_->setBackgroundColor(0, Qt::white);
+	}
+
 	ElementsLocation selected_location(selectedElementLocation());
 	if (!selected_location.isNull()) {
 		return(QETApp::collectionItem(selected_location));
@@ -199,7 +204,18 @@ void ElementsPanel::dragMoveEvent(QDragMoveEvent *e) {
 		return;
 	}
 #endif
-	
+
+	// Change color on mouse over
+	it_ = itemAt(e -> pos());
+	if(it_prev_){
+			if(it_ != it_prev_){
+			it_->setBackgroundColor(0, Qt::cyan);
+			it_prev_->setBackgroundColor(0, Qt::white);
+			it_prev_ = it_;
+		}
+	}
+	else it_prev_ = it_;
+
 	e -> accept();
 	/// @todo mettre en valeur le lieu de depot
 }
