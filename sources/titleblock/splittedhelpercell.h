eaff56b6692c7663051411d664026559157d9aef
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2015-02-20 14:56:22 +0000

    Update copyright date
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@3734 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/titleblock/splittedhelpercell.h
+++ sources/titleblock/splittedhelpercell.h
@@ -1,5 +1,5 @@
 /*
-    Copyright 2006-2014 The QElectroTech Team
+    Copyright 2006-2015 The QElectroTech Team
     This file is part of QElectroTech.
     
     QElectroTech is free software: you can redistribute it and/or modify
