025fa64f67d7609bebd3a0d522739562c71498f7
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2016-05-13 17:40:36 +0000

    Minor : update Copyright date
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@4481 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/ui/shapegraphicsitempropertieswidget.h
+++ sources/ui/shapegraphicsitempropertieswidget.h
@@ -1,5 +1,5 @@
 /*
-	Copyright 2006-2015 The QElectroTech Team
+	Copyright 2006-2016 The QElectroTech Team
 	This file is part of QElectroTech.
 
 	QElectroTech is free software: you can redistribute it and/or modify
