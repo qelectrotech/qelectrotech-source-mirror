fd951821afeedb2aea8c0375ca4031fe1289feee
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2016-08-20 12:52:57 +0000

    GenericPanel : add switch for folio ID or folio label
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@4641 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/genericpanel.cpp
+++ sources/genericpanel.cpp
@@ -282,12 +282,26 @@ QTreeWidgetItem *GenericPanel::getItemForDiagram(Diagram *diagram, bool *created
 QTreeWidgetItem *GenericPanel::updateDiagramItem(QTreeWidgetItem *diagram_qtwi, Diagram *diagram, PanelOptions options, bool freshly_created) {
 	Q_UNUSED(options)
 	if (!diagram || !diagram_qtwi) return(0);
+	QSettings settings;
 	
 	QString displayed_title = diagram -> title();
 	if (displayed_title.isEmpty()) {
-        displayed_title = tr("Folio sans titre", "Fallback label when a diagram has no title");
+		displayed_title = tr("Folio sans titre", "Fallback label when a diagram has no title");
+	}
+	if (settings.value("genericpanel/folio", true).toBool()){
+	QString displayed_label = diagram ->border_and_titleblock.folio();
+	int diagram_folio_idx = diagram -> folioIndex();
+	if (diagram_folio_idx != -1) {
+		displayed_label = QString(
+			tr(
+				"%1 - %2",
+				"label displayed for a diagram in the panel ; %1 is the folio index, %2 is the diagram title"
+			)
+		).arg(displayed_label).arg(displayed_title);
+		diagram_qtwi -> setText(0, displayed_label);
 	}
 	
+	}else{
 	QString displayed_label;
 	int diagram_folio_idx = diagram -> folioIndex();
 	if (diagram_folio_idx != -1) {
@@ -297,12 +311,11 @@ QTreeWidgetItem *GenericPanel::updateDiagramItem(QTreeWidgetItem *diagram_qtwi, 
 				"label displayed for a diagram in the panel ; %1 is the folio index, %2 is the diagram title"
 			)
 		).arg(diagram_folio_idx + 1).arg(displayed_title);
-	} else {
-		displayed_label = displayed_title;
 	}
 	
-	
 	diagram_qtwi -> setText(0, displayed_label);
+	
+	}
 	if (freshly_created) {
 		diagram_qtwi -> setData(0, GenericPanel::Item, qVariantFromValue(diagram));
 		diagrams_.insert(diagram, diagram_qtwi);
@@ -314,7 +327,9 @@ QTreeWidgetItem *GenericPanel::updateDiagramItem(QTreeWidgetItem *diagram_qtwi, 
 	}
 	
 	return(updateItem(diagram_qtwi, options, freshly_created));
-}
+	}
+
+
 
 /**
 	
