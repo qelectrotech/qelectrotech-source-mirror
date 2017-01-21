f8f9082a41e5a03901aedebb0a983cc5b355c389
nuri <nuri@r5884  nuri  2019-05-21 08:29:23 +0200 (mar. 21 mai 2019)>
2017-01-21 08:12:06 +0000

    Updated german translation
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@4864 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- lang/qet_de.ts
+++ lang/qet_de.ts
@@ -61,7 +61,7 @@
         <location filename="../sources/aboutqet.cpp" line="106"/>
         <source>2006-2017 Les développeurs de QElectroTech</source>
         <comment>about tab, developers line</comment>
-        <translation type="unfinished">2006-2016 die QElectroTech Entwickler {2006-2017 ?}</translation>
+        <translation>2006-2017 die QElectroTech Entwickler</translation>
     </message>
     <message>
         <location filename="../sources/aboutqet.cpp" line="110"/>
