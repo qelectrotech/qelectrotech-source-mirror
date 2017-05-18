9c8b1a7371416cf38e01add1a92dcbcc135a8e82
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2017-05-18 16:34:37 +0000

    Disable High Dpi Scaling
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@4965 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/main.cpp
+++ sources/main.cpp
@@ -31,8 +31,10 @@ int main(int argc, char **argv)
 	QCoreApplication::setOrganizationDomain("qelectrotech.org");
 	QCoreApplication::setApplicationName("QElectroTech");
 		//Creation and execution of the application
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
-QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
+#if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
+    QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+#else
+    qputenv("QT_DEVICE_PIXEL_RATIO", QByteArray("1"));
+#endif 
 	return(QETApp(argc, argv).exec());
 }
