df48158c300d8c678f050e3e9ec9ff1a8576a9eb
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2018-01-12 22:02:39 +0000

    Change displayed Version to "0.60-RC4"
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/branches/0.60@5214 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/qet.h
+++ sources/qet.h
@@ -27,7 +27,7 @@ namespace QET {
 	/// QElectroTech version (as string, used to mark projects and elements XML documents)
 	const QString version = "0.60";
 	/// QElectroTech displayed version
-	const QString displayedVersion = "0.60-RC3";
+	const QString displayedVersion = "0.60-RC4";
 	QString license();
 	
 	/// Oriented movements
