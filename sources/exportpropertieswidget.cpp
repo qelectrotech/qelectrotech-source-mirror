d8894809ad9402c1d0bbae502a0acd7ae1e2c0e4
scorpio810 <scorpio810@r995  scorpio810  2010-05-20 11:17:04 +0200 (jeu. 20 mai 2010)>
2019-03-16 17:59:17 +0000

    The DXF export is broken now on 0.7-dev, hide button in export dialog but keeps the code if anyone wants to work on it
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/qet/qet/trunk@5793 bfdf4180-ca20-0410-9c96-a3a8aa849046

--- sources/exportpropertieswidget.cpp
+++ sources/exportpropertieswidget.cpp
@@ -145,7 +145,7 @@ void ExportPropertiesWidget::build() {
 	format -> addItem(tr("JPEG (*.jpg)"),   "JPG");
 	format -> addItem(tr("Bitmap (*.bmp)"), "BMP");
 	format -> addItem(tr("SVG (*.svg)"),    "SVG");
-	format -> addItem(tr("DXF (*.dxf)"),    "DXF");
+	//format -> addItem(tr("DXF (*.dxf)"),    "DXF");
 	hboxLayout1 -> addStretch();
 	
 	vboxLayout -> addLayout(hboxLayout1);
