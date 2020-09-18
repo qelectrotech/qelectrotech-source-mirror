813014ec3378234fba17748a862441f7e5de1f37
Simon De Backer <simondebacker@msn.com>
2020-09-18 23:05:47 +0200

    Fix deprecated QRegExp
    Use QRegularExpression instead.
    
    https://doc.qt.io/qt-5/qregularexpression.html#notes-for-qregexp-users
    
    This function was introduced in Qt 5

--- sources/titleblock/templatelocation.cpp
+++ sources/titleblock/templatelocation.cpp
@@ -1,23 +1,25 @@
 /*
 	Copyright 2006-2020 The QElectroTech Team
 	This file is part of QElectroTech.
-	
+
 	QElectroTech is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 2 of the License, or
 	(at your option) any later version.
-	
+
 	QElectroTech is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with QElectroTech.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "templatelocation.h"
 #include "templatescollection.h"
 #include "qetapp.h"
+
+#include <QRegularExpression>
 
 // make this class usable with QVariant
 int TitleBlockTemplateLocation::MetaTypeId = qRegisterMetaType<TitleBlockTemplateLocation>("TitleBlockTemplateLocation");
@@ -97,10 +99,10 @@ bool TitleBlockTemplateLocation::isValid() const
 */
 void TitleBlockTemplateLocation::fromString(const QString &loc_str) {
 	collection_ = QETApp::titleBlockTemplatesCollection(QUrl(loc_str).scheme());
-	
-	QRegExp name_from_url("^[^:]*:\\/\\/(.*)$");
-	if (name_from_url.exactMatch(loc_str)) {
-		name_ = name_from_url.capturedTexts().at(1);
+
+	QRegularExpression name_from_url("^[^:]*:\\/\\/(.*)$");
+	if (name_from_url==QRegularExpression(loc_str)) {
+		name_ = name_from_url.namedCaptureGroups().at(1);
 	} else {
 		name_ = QString();
 	}
