e878082b795c3f04a866f4028d37b43501b730ef
Claveau Joshua <Joshua@qelectrotech.org>
2020-10-10 16:58:02 +0200

    Fix bug 203
    https://qelectrotech.org/bugtracker/view.php?id=203
    Take advantage of this bugfix, to recode the widget with Qt designer.

--- sources/editor/ui/ellipseeditor.h
+++ sources/editor/ui/ellipseeditor.h
@@ -1,72 +1,67 @@
 /*
 	Copyright 2006-2020 The QElectroTech Team
 	This file is part of QElectroTech.
-	
+
 	QElectroTech is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 2 of the License, or
 	(at your option) any later version.
-	
+
 	QElectroTech is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with QElectroTech.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef ELLIPSE_EDITOR_H
-#define ELLIPSE_EDITOR_H
+#ifndef ELLIPSEEDITOR_H
+#define ELLIPSEEDITOR_H
 
 #include "elementitemeditor.h"
 
+namespace Ui {
+	class EllipseEditor;
+}
+
 class PartEllipse;
 class StyleEditor;
-class QDoubleSpinBox;
 
 /**
-	This class provides a widget to edit ellipses within the element editor.
-*/
+ * @brief The EllipseEditor class
+ * This class provide a widget used to edit the properties of a @PartEllipse
+ */
 class EllipseEditor : public ElementItemEditor
 {
-	Q_OBJECT
+    Q_OBJECT
 
-		// constructors, destructor
 	public:
-		EllipseEditor(QETElementEditor *, PartEllipse * = nullptr, QWidget * = nullptr);
+		explicit EllipseEditor (QETElementEditor *editor, PartEllipse *part = nullptr, QWidget *parent = nullptr);
 		~EllipseEditor() override;
-	private:
-		EllipseEditor(const EllipseEditor &);
-	
-		// attributes
-	private:
-		PartEllipse *part;
-		StyleEditor *style_;
-		QDoubleSpinBox *x, *y, *h, *v;
-		bool m_locked;
-		QList <QMetaObject::Connection> m_change_connections;
-	
-		// methods
-	public:
-		bool setPart(CustomElementPart *) override;
-		bool setParts(QList <CustomElementPart *> parts) override;
+
+		bool setPart(CustomElementPart *part) override;
+		bool setParts(QList<CustomElementPart *> parts) override;
 		CustomElementPart *currentPart() const override;
 		QList<CustomElementPart*> currentParts() const override;
-	
-	public slots:
-		void editingFinishedX();
-		void editingFinishedY();
-		void editingFinishedH();
-		void editingFinishedV();
 		void updateForm() override;
-	
+
+	private slots:
+		void on_m_x_sb_editingFinished();
+		void on_m_y_sb_editingFinished();
+		void on_m_horizontal_diameter_sb_editingFinished();
+		void on_m_vertical_diameter_sb_editingFinished();
+
 	private:
-		void activeConnections(bool);
-		/**
-			@brief setUpChangeConnections
-			Setup the connection from the ellipse(s) to the widget, to update it when the ellipse(s) are changed (moved ...)
-		*/
+		void updateRect();
 		void setUpChangeConnections();
 		void disconnectChangeConnections();
+
+	private:
+		Ui::EllipseEditor *ui;
+		PartEllipse *m_part = nullptr;
+		StyleEditor *m_style = nullptr;
+		QList <QMetaObject::Connection> m_change_connections;
+
 };
-#endif
+
+#endif // ELLIPSEEDITOR_H
