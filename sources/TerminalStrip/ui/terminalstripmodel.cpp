8c554e875dcb411cc63d319159b9b01a37c0a6a3
joshua <Joshua@qelectrotech.org>
2021-09-10 23:43:36 +0200

    Use QLatin1String

--- sources/TerminalStrip/ui/terminalstripmodel.cpp
+++ sources/TerminalStrip/ui/terminalstripmodel.cpp
@@ -353,7 +353,7 @@ QWidget *TerminalStripModelDelegate::createEditor(QWidget *parent, const QStyleO
 {
 	if (index.column() == TYPE_CELL) {
 		auto qcb = new QComboBox(parent);
-		qcb->setObjectName("terminal_type");
+		qcb->setObjectName(QLatin1String("terminal_type"));
 		qcb->addItem(ElementData::translatedTerminalType(ElementData::TTGeneric),   ElementData::TTGeneric);
 		qcb->addItem(ElementData::translatedTerminalType(ElementData::TTFuse),      ElementData::TTFuse);
 		qcb->addItem(ElementData::translatedTerminalType(ElementData::TTSectional), ElementData::TTSectional);
@@ -364,7 +364,7 @@ QWidget *TerminalStripModelDelegate::createEditor(QWidget *parent, const QStyleO
 	}
 	if (index.column() == FUNCTION_CELL) {
 		auto qcb = new QComboBox(parent);
-		qcb->setObjectName("terminal_function");
+		qcb->setObjectName(QLatin1String("terminal_function"));
 		qcb->addItem(ElementData::translatedTerminalFunction(ElementData::TFGeneric), ElementData::TFGeneric);
 		qcb->addItem(ElementData::translatedTerminalFunction(ElementData::TFPhase),   ElementData::TFPhase);
 		qcb->addItem(ElementData::translatedTerminalFunction(ElementData::TFNeutral), ElementData::TFNeutral);
