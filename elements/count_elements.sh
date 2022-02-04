d74dcdea6c7533e7850bf17e9507e434ecf4e530
Laurent Trinques <scorpio@qelectrotech.org>
2022-02-04 16:16:34 +0100

    Minor

--- elements/count_elements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# compte les elements
-nb_elmts=$(find . -name "*.elmt" | wc -l)
-nb_categ=$(find . -name "qet_directory" | wc -l)
-((nb_files=$nb_elmts + $nb_categ))
-
-echo "${nb_elmts} elements dans ${nb_categ} categories (soit ${nb_files} fichiers)"
