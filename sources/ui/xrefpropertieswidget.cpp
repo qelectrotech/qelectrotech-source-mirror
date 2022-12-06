4c89b7157c8d5c775dc70d124f6592177a8efb42
luz paz <luzpaz@pm.me>
2022-12-06 06:08:21 -0500

    Fix source typo 'Rigth'

--- sources/ui/xrefpropertieswidget.cpp
+++ sources/ui/xrefpropertieswidget.cpp
@@ -108,7 +108,7 @@ void XRefPropertiesWidget::buildUi()
 	ui -> m_xrefpos_cb -> addItem(tr("Top"),"top");
 	ui -> m_xrefpos_cb -> addItem(tr("Bottom"),"bottom");
 	ui -> m_xrefpos_cb -> addItem(tr("Left"),"left");
-	ui -> m_xrefpos_cb -> addItem(tr("Rigth"),"right");
+	ui -> m_xrefpos_cb -> addItem(tr("Right"),"right");
 	ui -> m_xrefpos_cb -> addItem(tr("Text alignment"),"alignment");
 	m_previous_type_index = ui -> m_type_cb -> currentIndex();
 }
