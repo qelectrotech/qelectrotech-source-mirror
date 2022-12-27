66614072a6a82ed893aeeb8fc3223a4e796f28cf
Laurent Trinques <scorpio@qelectrotech.org>
2022-12-27 17:36:59 +0100

    Typo

--- sources/ui/aboutqetdialog.cpp
+++ sources/ui/aboutqetdialog.cpp
@@ -128,10 +128,10 @@ void AboutQETDialog::setTranslators()
 	addAuthor(ui->m_translators_label, "Gábor Gubányi",                             "gubanyig@gmail.com",             tr("Traduction en hongrois"));
 	addAuthor(ui->m_translators_label, "",                                          "",                               tr("Traduction en serbe"));
 	addAuthor(ui->m_translators_label, "Yaroslav",                                  "",                               tr("Traduction en ukrainien"));
-	addAuthor(ui->m_translators_label, "JoelAs",                                    "",                               tr("Traduction en norvegien"));
+	addAuthor(ui->m_translators_label, "JoelAs",                                    "",                               tr("Traduction en norvégien"));
 	addAuthor(ui->m_translators_label, "Yuki",                                      "yuki.atoh@gmail.com",            tr("Traduction en japonais"));
 	addAuthor(ui->m_translators_label, "Nathalie",                                  "nathalie.roussier@giz.de",       tr("Traduction en mongol"));
-	addAuthor(ui->m_translators_label, "Uroš Platiše",                              "uros.platise@energycon.eu",      tr("Traduction en slovene"));
+	addAuthor(ui->m_translators_label, "Uroš Platiše",                              "uros.platise@energycon.eu",      tr("Traduction en slovène"));
 }
 
 /**
