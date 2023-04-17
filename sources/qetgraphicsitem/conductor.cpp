247b98da41eaeafcae6a344c67a581544ce6bba9
joshua <Joshua@qelectrotech.org>
2023-04-17 22:09:18 +0200

    Fix minor bug : conductor text is not updated when folio id is used
    See https://qelectrotech.org/forum/viewtopic.php?pid=18157#p18157

--- sources/qetgraphicsitem/conductor.cpp
+++ sources/qetgraphicsitem/conductor.cpp
@@ -1765,25 +1765,29 @@ void Conductor::setSequenceNum(const autonum::sequentialNumbers& sn)
 */
 void Conductor::setUpConnectionForFormula(QString old_formula, QString new_formula)
 {
-	if (diagram())
+    Diagram *diagram_ {diagram()};
+    if (!diagram_) {
+        diagram_ = terminal1->diagram();
+    }
+    if (diagram_)
 	{
 			//Because the variable %F is a reference to another text which can contain variables,
 			//we must replace %F by the real text, to check if the real text contains the variable %id
 		if (old_formula.contains("%F"))
-			old_formula.replace("%F", diagram()->border_and_titleblock.folio());
+            old_formula.replace("%F", diagram_->border_and_titleblock.folio());
 
 		if (old_formula.contains("%id"))
-			disconnect(diagram()->project(), &QETProject::projectDiagramsOrderChanged, this, &Conductor::refreshText);
+            disconnect(diagram_->project(), &QETProject::projectDiagramsOrderChanged, this, &Conductor::refreshText);
 
 			//Label is frozen, so we don't update it.
 		if (m_freeze_label == true)
 			return;
 
 		if (new_formula.contains("%F"))
-			new_formula.replace("%F", diagram()->border_and_titleblock.folio());
+            new_formula.replace("%F", diagram_->border_and_titleblock.folio());
 
 		if (new_formula.contains("%id"))
-			connect(diagram()->project(), &QETProject::projectDiagramsOrderChanged, this, &Conductor::refreshText);
+            connect(diagram_->project(), &QETProject::projectDiagramsOrderChanged, this, &Conductor::refreshText);
 	}
 }
 
