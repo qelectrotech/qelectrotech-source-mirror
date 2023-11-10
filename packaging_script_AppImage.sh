5e763bc8d2aab84c5d651d7f7f07fd114b7ae1b8
Laurent Trinques <scorpio@qelectrotech.org>
2023-11-10 14:29:21 +0100

    Update packaging_script_AppImage.sh

--- packaging_script_AppImage.sh
+++ packaging_script_AppImage.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-#set -x
+#!/bin/bash 
+#nettoyage des chroots
+set -x
+
 
 #delete old qet.h
 rm sources/qet.h
@@ -8,11 +10,11 @@ cd sources
 git reset --hard origin/master
 
 cd ..
-# get updates
-git submodule init
-git submodule update
-git pull --recurse-submodules
+
+# Fait une mise à jour
+git submodule update --init --recursive
 git pull
+#git checkout test_pugi
 
 GITCOMMIT=$(git rev-parse --short HEAD)
 A=$(git rev-list HEAD --count)
@@ -44,3 +46,22 @@ rm QElectroTech_*.AppImage
 ARCH=x86_64 ./appimagetool-x86_64.AppImage qelectrotech
 chmod -x QElectroTech_$tagName-r$HEAD-x86_64.AppImage
 shasum -a 256 QElectroTech_$tagName-r$HEAD-x86_64.AppImage > QElectroTech_$tagName-r$HEAD-x86_64.AppImage-SHA256.txt
+mv QElectroTech_$tagName-r$HEAD-x86_64.AppImage* ../AppImage/0.100.0/
+cd ..
+  #rsync to TF
+  echo -e "\033[1;31mWould you like to RSYNC Debian packages to TF n/Y?.\033[m"
+  read a
+  if [[ $a == "Y" || $a == "y" ]]; then
+    echo -e "\033[1;33mRsync to TF qelectrotech-$VERSION.r$HEAD .\033[m"
+    echo -e	"\033[1;31mrsync to TF password ssh and TF\033[m"
+    cd $DEFAULT_DIR/script
+    rsync -e ssh -av --delete-after --no-owner --no-g --chmod=g+w --progress ~/qelectrotech-source-mirror/AppImage/0.100.0/ server:download.qelectrotech.org/qet/builds/AppImage/0.100.0/
+    if [ $? != 0 ]; then
+    {
+      echo "RSYNC ERROR: problem syncing qelectrotech-$VERSION.r$HEAD "
+      rsync -e ssh -av --delete-after --no-owner --no-g --chmod=g+w --progress  ~/qelectrotech-source-mirror/AppImage/0.100.0/ server:download.qelectrotech.org/qet/builds/AppImage/0.100.0/
+    } fi
+
+  else
+    echo -e "\033[1;33mExit.\033[m"
+  fi
