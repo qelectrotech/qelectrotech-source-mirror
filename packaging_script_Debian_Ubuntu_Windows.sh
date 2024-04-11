b453832d46733383cd70fe78403cc7aa0df2c628
Laurent Trinques <scorpio@qelectrotech.org>
2024-04-11 15:54:48 +0200

    Update packaging_script_Debian_Ubuntu_Windows.sh

--- packaging_script_Debian_Ubuntu_Windows.sh
+++ packaging_script_Debian_Ubuntu_Windows.sh
@@ -31,7 +31,8 @@ if [ -z "$HEAD" ] ; then
 
 fi
 
-VERSION=$(cat sources/qetversion.cpp | grep "return QVersionNumber{ 0, "| head -n 1| cut -c32-40| sed -e 's/,/./g' -e 's/ //g')   #Find major, minor, and micro version numbers in sources/qetversion.cp
+VERSION=$(cat sources/qetversion.cpp | grep "return QVersionNumber{"| head -n 1| awk -F "{" '{ print $2 }' | awk -F "}" '{ print $1 }' | sed -e 's/,/./g' -e 's/ //g')
+#VERSION=$(cat sources/qetversion.cpp | grep "return QVersionNumber{ 0, "| head -n 1| cut -c32-40| sed -e 's/,/./g' -e 's/ //g')   #Find major, minor, and micro version numbers in sources/qetversion.cp
 
 
 #clean /var/cache/pbuilder/ .. / result/debs
