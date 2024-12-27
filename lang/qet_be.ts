d6dcc85157ebc450dce0e7d502fd09db60dfafdc
Laurent Trinques <scorpio@qelectrotech.org>
2024-12-27 14:59:06 +0100

    Update qet_be.ts

--- lang/qet_be.ts
+++ lang/qet_be.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl_NL">
+<TS version="2.1" language="nl_BE">
 <context>
     <name>AboutQETDialog</name>
     <message>
