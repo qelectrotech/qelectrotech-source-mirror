1c2d83d586b8e16f280ef7ca5dd11c4ff45476c9
Magnus Hellströmer <magnus.hellstromer@gmail.com>
2025-02-15 23:18:14 +0100

    Fix compile error

--- sources/machine_info.cpp
+++ sources/machine_info.cpp
@@ -188,7 +188,7 @@ void MachineInfo::send_info_to_debug()
 	QDirIterator it1(QETApp::commonElementsDir().toLatin1(),nameFilters,  QDir::Files, QDirIterator::Subdirectories);
 			while (it1.hasNext())
 			{
-				if(it1.next() > 0 )
+				if(it1.next() != "")
 				{
 				commomElementsDir ++;
 				}
@@ -200,7 +200,7 @@ void MachineInfo::send_info_to_debug()
 	QDirIterator it2(QETApp::customElementsDir().toLatin1(), nameFilters, QDir::Files, QDirIterator::Subdirectories);
 			while (it2.hasNext())
 			{
-				if(it2.next() > 0 )
+				if(it2.next() != "")
 				{
 				customElementsDir ++;
 				}
@@ -211,7 +211,7 @@ void MachineInfo::send_info_to_debug()
 	QDirIterator it3(QETApp::companyElementsDir().toLatin1(), nameFilters, QDir::Files, QDirIterator::Subdirectories);
 			while (it3.hasNext())
 			{
-				if(it3.next() > 0 )
+				if(it3.next() != "")
 				{
 				companyElementsDir ++;
 				}
