e2e84a56f7dd7ab272c3002a48e08991c2e06bf1
plc-user <74435298+plc-user@users.noreply.github.com>
2025-02-28 07:40:59 +0100

    limit movement to int-values to be able to re-position to original position

--- sources/qetgraphicsitem/dynamicelementtextitem.cpp
+++ sources/qetgraphicsitem/dynamicelementtextitem.cpp
@@ -584,7 +584,9 @@ void DynamicElementTextItem::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 			current_parent_pos = mapToParent(mapFromScene(event->scenePos()));
 			button_down_parent_pos = mapToParent(mapFromScene(event->buttonDownScenePos(Qt::LeftButton)));
 			
-			QPointF new_pos = m_initial_position + current_parent_pos - button_down_parent_pos;
+			int diffx = qRound(current_parent_pos.x() - button_down_parent_pos.x());
+			int diffy = qRound(current_parent_pos.y() - button_down_parent_pos.y());
+			QPointF new_pos = m_initial_position + QPointF(diffx, diffy);
 			setPos(new_pos);
 
 			if(diagram())
