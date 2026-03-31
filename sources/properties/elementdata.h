c220d84fcb011430c539825eda88ae9e407b3125
Laurent Trinques <scorpio@qelectrotech.org>
2026-03-31 16:23:05 +0200

    Update elementdata.h

--- sources/properties/elementdata.h
+++ sources/properties/elementdata.h
@@ -134,10 +134,9 @@ class ElementData : public PropertiesInterface
 		ElementData::Type       m_type = ElementData::Simple;
 
 		ElementData::MasterType m_master_type = ElementData::Coil;
-<<<<<<< master
+
 		int m_max_slaves{-1};
-=======
->>>>>>> master
+
 
 		ElementData::SlaveType  m_slave_type  = ElementData::SSimple;
 		ElementData::SlaveState m_slave_state = ElementData::NO;
