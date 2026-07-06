82cd4d359a98cdc92e960f7f38087ba5b43ec99e
Laurent Trinques <scorpio@qelectrotech.org>
2026-07-06 14:45:58 +0200

    macOS fix: add explicit QGraphicsLayoutItem include in helpercell.h
    MOC on macOS does not resolve QGraphicsLayoutItem through the bulk
    QtWidgets include, causing an 'Undefined interface' error at build time.
    Adding an explicit include resolves this. Linux builds are unaffected. Thanks hairykiwi 8ef4e04

--- sources/titleblock/helpercell.h
+++ sources/titleblock/helpercell.h
@@ -20,7 +20,7 @@
 #include "../qet.h"
 
 #include <QtWidgets>
-
+#include <QGraphicsLayoutItem>
 /**
 	This class implements a helper widget for cells that indicate the length of
 	columns and rows.
